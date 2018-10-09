7a5beacad88505b134e6708ffc58e2c6b90f4efe
jobema51 <37087560+jobema51@users.noreply.github.com>
2018-10-09 18:29:20 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+ var map = L.map('mapa').setView([-25.306864, -57.609183], 17);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-25.306864, -57.609183]).addTo(map)
+    .bindPopup('jbmopen.com')
+    .openPopup();
